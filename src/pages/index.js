9d86b523c8cc0c664f277da75c362c106e7d88a3
Paul Reynolds <p.reynolds@akanewmedia.com>
2022-06-29 15:43:09 -0400

    tutorial pt 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making a Gatsby site via tutorial.</p>
+      <StaticImage
+        alt="an opossum under a canoe"
+        src="../images/opossom.jpg"
+      />
     </Layout>
   )
 }
